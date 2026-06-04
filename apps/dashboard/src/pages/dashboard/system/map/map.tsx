b29a26f5b54620257d2c8864c094fa96a854b632
Jonathan2745 <jonathan.joe2745@gmail.com>
2026-06-05 01:27:21 +0800

    feat(backend): added test data to backend

--- apps/dashboard/src/pages/dashboard/system/map/map.tsx
+++ apps/dashboard/src/pages/dashboard/system/map/map.tsx
@@ -1,8 +1,22 @@
-// import { SceneViewer } from './components/scene-viewer';
-import { SceneViewer } from './components/new-scene-viewer';
+// import { SceneViewer } from './components/new-scene-viewer';
+// TODO: Refactor to segregate the scene from the rest of the page
 
+import { Center, Spinner } from '@chakra-ui/react';
+import { lazy, Suspense } from 'react';
+
+const SceneViewer = lazy(() => import('./components/new-scene-viewer'));
 export function Map() {
-  return <SceneViewer />;
+  return (
+    <Suspense
+      fallback={
+        <Center h="100vh">
+          <Spinner />
+        </Center>
+      }
+    >
+      <SceneViewer />
+    </Suspense>
+  );
 }
 
 export default Map;
