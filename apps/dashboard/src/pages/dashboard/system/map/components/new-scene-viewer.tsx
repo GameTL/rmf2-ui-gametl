23a5c7666b644b024e8bb7b8740761ee5e5a5cb8
Jonathan2745 <jonathan.joe2745@gmail.com>
2026-06-05 10:35:25 +0800

    fix(refactor): abstracted data types to robot-types.tsx

--- apps/dashboard/src/pages/dashboard/system/map/components/new-scene-viewer.tsx
+++ apps/dashboard/src/pages/dashboard/system/map/components/new-scene-viewer.tsx
@@ -35,6 +35,9 @@ import {
   type SceneBounds,
   type DropPointCoords,
   type StaticCollisionBox,
+  type RobotTrailRuntime,
+  type SceneDebugInfo,
+  type LoadState,
 } from './robot-types';
 
 const SCENE_URL = '/RMF2_SIM/Test_3.glb';
